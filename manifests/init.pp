4561b513c7cceb7e9bff75fb44b684ad66ed22c5
Alexander Fisher <alex@linfratech.co.uk>
2016-09-02 12:54:44 +0100

    WIP/RFC: Add https_port defined type.

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,7 @@ class squid (
   $http_access                   = $squid::params::http_access,
   $auth_params                   = $squid::params::auth_params,
   $http_ports                    = $squid::params::http_ports,
+  $https_ports                   = $squid::params::https_ports,
   $snmp_ports                    = $squid::params::snmp_ports,
   $cache_dirs                    = $squid::params::cache_dirs,
 ) inherits ::squid::params {
@@ -47,6 +48,9 @@ class squid (
   }
   if $http_ports {
     validate_hash($http_ports)
+  }
+  if $https_ports {
+    validate_hash($https_ports)
   }
   if $snmp_ports {
     validate_hash($snmp_ports)
